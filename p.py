daa49be395c4ee41e967260c6d82d304f13f4c25
RochSchanen <r.schanen@lancaster.ac.uk>
2020-12-21 14:06:40 +0000

    init

---
+++ p.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# file p.py
+# created 20201221
+# author Roch Schanen
+
+if __name__ == "__main__":
+	print("""
+	# file p.py
+	# created 20201221
+	# author Roch Schanen
+	""")
