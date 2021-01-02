f51c8817b3729f8301ae3eb6c641053a4f460bd3
RochSchanen <r.schanen@lancaster.ac.uk>
2021-01-02 00:32:14 +0000

    add getCache, flushCache

--- virtualstorage.py
+++ virtualstorage.py
@@ -8,6 +8,7 @@ from sys import exit
 import numpy as np
 
 # 20201221 add virtual storage class
+# 20210101 create storage file
 
 __DEBUG__ = True
 # __DEBUG__ = False
@@ -105,21 +106,33 @@ class virtualstorage():
             error = True
         # exit on error        
         if error: exit()
+        # make header
+        self.header  = self.wordSize.to_bytes(4, 'little')
+        self.header += self.blockSize.to_bytes(4, 'little')
+        self.header += self.deviceSize.to_bytes(4, 'little')
         # compute file size in bits
-        bitFileSize  = self.wordSize * self.blockSize * self.deviceSize
+        bitFileSize  = self.wordSize * self.blockSize * self.deviceSize + 3*4*8
         # compute file size in bytes
         byteFileSize = bitFileSize >> 3
-        # ceil completion
+        # 8bits alignment
         if bitFileSize & 7: byteFileSize += 1  
         # create binary file to hold the data
-        self.fileHandle = open(self.filePath, "wb")
-        # set the file size
+        self.fileHandle = open(self.filePath, "wb+")
+        # write header
+        self.fileHandle.write(self.header)
+        # point to the end-of-file
         self.fileHandle.seek(byteFileSize - 1)
+        # write an arbitrary byte to fix the file size
         self.fileHandle.write(b"\x00")
         #done
         return
 
     def closeDevice(self):
+        if not self.fileHandle:
+            print('closeDevice error:')
+            print(' -> no device')
+            print(' -> no operation')
+            return
         # flush data
         # ...        
         # close
@@ -129,23 +142,59 @@ class virtualstorage():
         # done
         return
 
-    # blockAddress    = None      # current block address: pointer to a block
-    # blockData       = None      # cache data stored at the current block address
+    blockAddress  = None  # current block address: pointer to a block
+    blockData     = None  # cache data stored at the current block address
 
-    # def setAddress(self, a):
-    #     # check boundaries
-    #     if a > self.deviceSize:
-    #         print('setAddress error:')
-    #         print(' -> address exceeds device size')
-    #         print(' -> no operation')
-    #         return
-    #     # flush data
-    #     # ...        
-    #     # set address
-    #     self.blockAddress = a
+    def getCache(self, ba):
+        """ return a reference to a cache block (ndarray of uint8) 
+        ba is the block address value (uint)
+         - the device contains exactly "deviceSize" blocks
+         - address of first block is 0
+         - address of last block is deviceSize-1
+        """
+        if not self.fileHandle:
+            print('getCache warning:')
+            print(' -> no device')
+            print(' -> no operation')
+            return None
+        # check upper bound
+        if ba >= self.deviceSize:
+            print('getCache warning:')
+            print(' -> address exceeds device size')
+            print(' -> no operation')
+            return None
+        # flush data
+        # ...
+        # set address
+        self.blockAddress = ba
+        # set file pointer
+        self.fileHandle.seek(3*4 + ba*self.blockSize)
+        # read one block
+        d = self.fileHandle.read(self.blockSize)
+        # convert byte string into numpy array (mutable)
+        self.blockData = np.copy(np.frombuffer(d, np.uint8))
+        # done
+        return self.blockData # reference to a mutable ndarray
 
-    #     #done
-    #     return        
+    def flushCache(self):
+        if self.fileHandle is None:
+            print('flushCache warning:')
+            print(' -> no device')
+            print(' -> no operation')
+            return
+        if self.blockData is None:
+            print('flushCache warning:')
+            print(' -> no cache to flush')
+            print(' -> no operation')
+            return
+        # set file pointer
+        self.fileHandle.seek(3*4 + self.blockAddress*self.blockSize)
+        # convert numpy array to binary string
+        bs = self.blockData.tobytes()
+        # write block
+        d = self.fileHandle.write(bs)
+        # done        
+        return
 
 if __name__ == "__main__":
     print("""
@@ -158,7 +207,16 @@ if __name__ == "__main__":
 
     vs.setFilePath('./VS1MB')   # define a 1MB storage as an example
     vs.setWordSize(8)           # 8 bits
-    vs.setBlockSize(256)        # 256 words
-    vs.setDeviceSize(4096)      # 4096 blocks
+    vs.setBlockSize(16)         # 256 words
+    vs.setDeviceSize(16)        # 4096 blocks
     vs.createDevice()           # create the new storage
+
+    b1 = vs.getCache(0)
+    b1[0] = 16
+    vs.flushCache()
+
+    b1 = vs.getCache(15)
+    b1[0:16] = [255]*16
+    vs.flushCache()
+
     vs.closeDevice()            # close storage
